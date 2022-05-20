0e3ff35f577bc03d79d0292f23733405d02d4b25
Felipe Amorim <felipeamorimw@gmail.com>
2022-05-19 21:43:55 -0300

    + Method to update user as admin

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import User from '../models/UserModel';
 import Vehicle from '../models/VehicleModel';
@@ -10,6 +11,40 @@ class UserController {
       user.password = '';
     });
     return res.status(200).json({ users });
+  }
+
+  async updateUser(req: Request, res: Response): Promise<Response> {
+    const { id, nname, nemail, npassword, nrole } = req.body;
+    if (!id || !nname || !nemail || !npassword || !nrole) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const user = await User.find({ _id: id });
+    if (!user || user.length === 0) {
+      return res.status(400).json({ error: 'User does not exists' });
+    }
+
+    try {
+      const passwordhash = bcrypt.hashSync(npassword, 10);
+      const userUpdated = await User.findOneAndUpdate(
+        { _id: id },
+        {
+          name: nname,
+          email: nemail,
+          password: passwordhash,
+          role: nrole,
+        },
+        { returnDocument: 'after' }
+      );
+      if (userUpdated !== null) {
+        userUpdated.password = '';
+        return res.status(200).json({ userUpdated });
+      } else {
+        return res.status(400).json({ error: 'User does not exists' });
+      }
+    } catch {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async deleteUser(req: Request, res: Response): Promise<Response> {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,31 +15,43 @@ routes.put(
   AuthController.changePassword
 );
 
+// request users <- admin
 routes.get(
   '/users',
   AuthMiddleware.validateToken,
   UserMiddleware.validateAdminRole,
   UserController.getUsers
 );
+// update user <- admin
+routes.put(
+  '/users',
+  AuthMiddleware.validateToken,
+  UserMiddleware.validateAdminRole,
+  UserController.updateUser
+);
+// delete user <- admin
 routes.delete(
   '/users',
   AuthMiddleware.validateToken,
   UserMiddleware.validateAdminRole,
   UserController.deleteUser
 );
-routes.get(
-  '/vehicles',
-  AuthMiddleware.validateToken,
-  UserMiddleware.validateAdminRole,
-  UserController.getVehicles
-);
+// create vehicle <- admin
 routes.post(
   '/vehicles',
   AuthMiddleware.validateToken,
   UserMiddleware.validateAdminRole,
   UserController.createVehicle
 );
+// request vehicles <- admin
+routes.get(
+  '/vehicles',
+  AuthMiddleware.validateToken,
+  UserMiddleware.validateAdminRole,
+  UserController.getVehicles
+);
 
+// request self vehicles <- user
 routes.get(
   '/vehicles',
   AuthMiddleware.validateToken,
