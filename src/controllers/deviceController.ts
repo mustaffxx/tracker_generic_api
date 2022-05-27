76a3d7e24f907f177ab574ab941b254bb398f562
Felipe Amorim <felipeamorimw@gmail.com>
2022-05-26 22:42:55 -0300

    + All basic methods of device controller

--- src/controllers/deviceController.ts
+++ src/controllers/deviceController.ts
@@ -28,4 +28,60 @@ class DeviceController {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+
+  async getDevice(req: Request, res: Response): Promise<Response> {
+    const { key } = req.body;
+    if (!key) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const device = Device.findOne({ key });
+
+    return res.status(200).json({ device });
+  }
+
+  async updateDevice(req: Request, res: Response): Promise<Response> {
+    const { _id, uid, vid, dname } = req.body;
+    if (!_id || !uid || !vid) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const device = Device.findOne({ _id });
+    if (device === null) {
+      return res.status(404).json({ error: 'Device does not exists' });
+    }
+
+    try {
+      const updatedDevice = await Device.findOneAndUpdate(
+        { _id },
+        {
+          uid: uid,
+          vid: vid,
+          dname: dname,
+        },
+        { returnDocument: 'after' }
+      );
+      if (updatedDevice) {
+        return res.status(200).json({ updatedDevice });
+      } else {
+        return res.status(404).json({ error: 'Device does not exists' });
+      }
+    } catch {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  async deleteDevice(req: Request, res: Response): Promise<Response> {
+    const { _id } = req.body;
+    if (!_id) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    try {
+      await Device.deleteOne({ _id });
+      return res.status(200).json({ message: 'Device deleted successfully' });
+    } catch {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 }
